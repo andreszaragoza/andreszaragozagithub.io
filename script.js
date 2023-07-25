a825c72ebe7002aa784fd30bd6c1fbd18dad839a
andreszaragoza <zaragozaquintero@gmail.com>
2023-07-25 10:13:02 +0200

    Initial commit

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("Javascript");
+        habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("python");
         habilidades[3].classList.add("sql");
